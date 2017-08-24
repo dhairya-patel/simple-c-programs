e3a13f67bf8f60acdb53608844635ace84b7b446
Dhairya Patel <31182647+dpacpop@users.noreply.github.com>
2017-08-24 18:41:54 +0530

    Add files via upload

---
+++ area_of_circle.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+# include <conio.h>
+using namespace std;
+int main()
+{
+	double a= 0,b= 0;
+	cout<<"\n enter the radius-\n";
+	cin>>a;
+	b= (3.14*a*a);
+	cout<<"\n area of circle= "<<b;
+	return 0;
+}
